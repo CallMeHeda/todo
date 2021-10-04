dae506235687fcd11876d6adadc4bd66a9d57325
Fatima El-Amri <fatimaelamri789@gmail.com>
2021-10-04 17:24:01 +0200

    some litte modification

--- src/PHP_Files/todos.php
+++ src/PHP_Files/todos.php
@@ -3,7 +3,12 @@
             <div class="logout">
                 <a href="index.php?choix=authentification_stop"><i class="fas fa-sign-out-alt fa-lg"></i></a>
             </div>
-            <h1><span><?php echo $_SESSION['login']; ?>'s</span> TO DO List</h1>
+            <?php
+            if (substr($_SESSION['login'], -1) == 's') { ?>
+                <h1><span><?php echo $_SESSION['login']; ?></span> TO DO List</h1>
+            <?php } else { ?>
+                <h1><span><?php echo $_SESSION['login']; ?>'s</span> TO DO List</h1>
+            <?php } ?>
             <div class="form-container">
                 <form action="" method="POST" class="form_task">
                     <input type="text" class="tache" name="tache" id="tache" maxlength="80"
